9a918f7be1f5b5730a3407ff7237fdd66dd68e11
Agustin Gomez <agusleongomez@gmail.com>
2023-05-27 19:26:59 -0300

    created delete action

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -5,6 +5,13 @@ export const AppReducer = (state, action) => {
         ...state,
         transactions: [...state.transactions, action.payload],
       };
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
