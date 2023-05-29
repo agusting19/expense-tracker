238282fcf63bc95881a84f59cfbe831e4a352e71
Agustin Gomez <agusleongomez@gmail.com>
2023-05-29 20:43:11 -0300

    added color variant

--- src/components/transactions/TransactionItem.jsx
+++ src/components/transactions/TransactionItem.jsx
@@ -7,8 +7,21 @@ const TransactionItem = ({ transaction }) => {
     <li className="flex justify-between items-center bg-zinc-600 text-white px-3 py-1 rounded-lg mb-2 w-full">
       <p className="text-sm">{transaction.description}</p>
       <div>
-        <span>${transaction.amount}</span>
-        <button onClick={() => deleteTransaction(transaction.id)}>x</button>
+        {transaction.amount < 0 ? (
+          <span className="text-red-500 text-sm">
+            -${Math.abs(transaction.amount)}
+          </span>
+        ) : (
+          <span className="text-green-500 text-sm">
+            ${Math.abs(transaction.amount)}
+          </span>
+        )}
+        <button
+          className="text-lg text-red-500 ml-2"
+          onClick={() => deleteTransaction(transaction.id)}
+        >
+          x
+        </button>
       </div>
     </li>
   );
